4c59507e75c3d7499b49184d03ad63c156260c26
Alfeo Raymond <ngelime07@gmail.com>
2022-12-19 12:04:08 +0300

    Update snake.js

--- snake.js
+++ snake.js
@@ -1,22 +1,26 @@
 var canvas = document.getElementById('game');
 var context = canvas.getContext('2d');
-
-// the canvas width & height, snake x & y, and the apple x & y, all need to be a multiples of the grid size in order for collision detection to work
-// (e.g. 16 * 25 = 400)
 var grid = 16;
 var count = 0;
+var score=0;
+//reading  last score value 
+if(localStorage.score){
+  document.getElementById('score').innerHTML=localStorage.score; 
+}
+
+var max=0;  
 
 var snake = {
   x: 160,
   y: 160,
-
+  
   // snake velocity. moves one grid length every frame in either the x or y direction
   dx: grid,
   dy: 0,
-
+  
   // keep track of all grids the snake body occupies
   cells: [],
-
+  
   // length of the snake. grows when eating an apple
   maxCells: 4
 };
@@ -33,20 +37,17 @@ function getRandomInt(min, max) {
 
 // game loop
 function loop() {
+  
   requestAnimationFrame(loop);
-
   // slow game loop to 15 fps instead of 60 (60/15 = 4)
-  if (++count < 4) {
+  if (++count < 10) {
     return;
   }
-
   count = 0;
   context.clearRect(0,0,canvas.width,canvas.height);
-
   // move snake by it's velocity
   snake.x += snake.dx;
   snake.y += snake.dy;
-
   // wrap snake position horizontally on edge of screen
   if (snake.x < 0) {
     snake.x = canvas.width - grid;
@@ -54,7 +55,7 @@ function loop() {
   else if (snake.x >= canvas.width) {
     snake.x = 0;
   }
-
+  
   // wrap snake position vertically on edge of screen
   if (snake.y < 0) {
     snake.y = canvas.height - grid;
@@ -62,61 +63,68 @@ function loop() {
   else if (snake.y >= canvas.height) {
     snake.y = 0;
   }
-
   // keep track of where snake has been. front of the array is always the head
   snake.cells.unshift({x: snake.x, y: snake.y});
-
   // remove cells as we move away from them
   if (snake.cells.length > snake.maxCells) {
     snake.cells.pop();
   }
-
   // draw apple
   context.fillStyle = 'red';
   context.fillRect(apple.x, apple.y, grid-1, grid-1);
-
   // draw snake one cell at a time
   context.fillStyle = 'green';
   snake.cells.forEach(function(cell, index) {
-
+    
     // drawing 1 px smaller than the grid creates a grid effect in the snake body so you can see how long it is
-    context.fillRect(cell.x, cell.y, grid-1, grid-1);
-
+    context.fillRect(cell.x, cell.y, grid-1, grid-1);  
     // snake ate apple
     if (cell.x === apple.x && cell.y === apple.y) {
       snake.maxCells++;
-
-      // canvas is 400x400 which is 25x25 grids
+	  score+=1;
+    //saving score for next playing.
+    localStorage.setItem('score',score);
+	  //max=score;
+	  document.getElementById('score').innerHTML=score;
+	
+      // canvas is 400x400 which is 25x25 grids 
       apple.x = getRandomInt(0, 25) * grid;
       apple.y = getRandomInt(0, 25) * grid;
     }
-
     // check collision with all cells after this one (modified bubble sort)
-    for (var i = index + 1; i < snake.cells.length; i++) {
-
+    for (var i = index + 1; i < snake.cells.length; i++)
+	{
+      
       // snake occupies same space as a body part. reset game
-      if (cell.x === snake.cells[i].x && cell.y === snake.cells[i].y) {
-        snake.x = 160;
+      if (cell.x === snake.cells[i].x && cell.y === snake.cells[i].y) 
+	 { 
+	 
+	    if(score>max)
+	    {
+	     max=score;
+	    }
+    	snake.x = 160;
         snake.y = 160;
         snake.cells = [];
         snake.maxCells = 4;
         snake.dx = grid;
         snake.dy = 0;
-
+		//score=0;
         apple.x = getRandomInt(0, 25) * grid;
         apple.y = getRandomInt(0, 25) * grid;
+	    document.getElementById('high').innerHTML=max;
       }
     }
-  });
+  }  
+  );  
 }
-
 // listen to keyboard events to move the snake
 document.addEventListener('keydown', function(e) {
-  // prevent snake from backtracking on itself by checking that it's
+  // prevent snake from backtracking on itself by checking that it's 
   // not already moving on the same axis (pressing left while moving
   // left won't do anything, and pressing right while moving left
   // shouldn't let you collide with your own body)
-
+  
   // left arrow key
   if (e.which === 37 && snake.dx === 0) {
     snake.dx = -grid;
@@ -138,6 +146,21 @@ document.addEventListener('keydown', function(e) {
     snake.dx = 0;
   }
 });
-
 // start the game
 requestAnimationFrame(loop);
+
+
+
+
+function myFunction() {
+  document.getElementById('end_msg').innerHTML="";
+  alert('Press confirm to continue');
+}
+
+//stop playing
+$(document).ready(function(){
+		$('#btn_stop').click(function(){
+      document.getElementById('end_msg').innerHTML="Game stoped by player" ;
+      setTimeout(myFunction, 1000);	
+		});
+	});
